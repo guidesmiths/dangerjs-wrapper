e55765debd118fce653414232bee881009aaec6f
Daniel Alarcon <daniel.alarcon@guidesmiths.com>
2020-12-07 17:39:49 +0100

    feat: execute npx danger on child process

--- command.js
+++ command.js
@@ -1,1 +1,7 @@
-module.exports = require('danger/distribution/commands/danger-ci')
+#!/usr/bin/env node
+
+const { spawn } = require('child_process')
+const child = spawn('npx', ['danger', 'ci'])
+
+child.stdout.pipe(process.stdout)
+child.stderr.pipe(process.stderr)

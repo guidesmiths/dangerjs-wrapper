411f7ebfbc9dd300fb6a3bf1a693a77ce28d96b2
Roberto Hernández <roberto.hernandez@guidesmiths.com>
2021-02-16 16:05:39 +0100

    feat: add rules for PR review process

---
+++ src/rules/checkReviewers.test.ts
@@ -0,0 +1,25 @@
+import { checkPRReviewers } from "./checkReviewers"
+
+declare const global: any
+
+describe("Check reviewers tests", () => {
+  beforeEach(() => {
+    global.warn = jest.fn()
+    global.message = jest.fn()
+    global.fail = jest.fn()
+    global.markdown = jest.fn()
+  })
+
+  afterEach(() => {
+    global.warn.mockClear()
+    global.message.mockClear()
+    global.fail.mockClear()
+    global.markdown.mockClear()
+  })
+
+  it("fails if there are no asignees on the PR", () => {
+    global.danger = { github: { requested_reviewers: { users: [] } } }
+    checkPRReviewers()
+    expect(global.fail).toHaveBeenCalled()
+  })
+})

e33406d648526c2127d9083586f883c9dcb3dd91
Roberto Hernández <roberto.hernandez@guidesmiths.com>
2021-02-16 19:52:55 +0100

    test(local): modify the message of the function to check pr reviewers
    modify the message of the function to check pr reviewers

--- src/rules/checkReviewers.ts
+++ src/rules/checkReviewers.ts
@@ -8,6 +8,6 @@ export declare function markdown(message: string): void
 
 export const checkPRReviewers = () => {
   if (danger.github.requested_reviewers?.users.length === 0) {
-    return fail("PR must have at least 1 reviewer!! 2")
+    return fail("PR must have at least 1 reviewer!!")
   }
 }

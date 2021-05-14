5b043ea54f6c549de91acb2a071cc49c912872d6
Roberto Hernández <roberto.hernandez@guidesmiths.com>
2021-05-14 19:09:46 +0200

    fix: function to add gifs

--- src/rules/checkFlawlessCode.ts
+++ src/rules/checkFlawlessCode.ts
@@ -6,10 +6,10 @@ export declare function warn(message: string): void
 export declare function fail(message: string): void
 
 const getGif = async (apiKey:string) => {
-  const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=lgtm&limit=25&offset=0&rating=g&lang=en`
+  const url = `https://api.giphy.com/v1/gifs/random?api_key=${apiKey}&q=lgtm&rating=g&lang=en`
   const response = await fetch(url)
   const responseJson = await response.json()
-  return responseJson.data[0].embed_url
+  return responseJson.data.url
 }
 
 export const checkFlawlessCode = (apiKey:string) => {

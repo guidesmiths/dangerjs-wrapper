98670e57a02c56a4dd57efdce4821f7126b3f581
Roberto Hernández <roberto.hernandez@guidesmiths.com>
2021-05-14 19:30:13 +0200

    fix: gif function and use axios rather than node fetch

--- src/rules/checkFlawlessCode.ts
+++ src/rules/checkFlawlessCode.ts
@@ -1,15 +1,14 @@
-import fetch from 'node-fetch'
+import axios from 'axios'
 
-// TODO Check how is this variabl// TODO Check where are declared these exported functions
+// TODO Check where are declared these exported functions
 export declare function message(message: string): void
 export declare function warn(message: string): void
 export declare function fail(message: string): void
 
 const getGif = async (apiKey:string) => {
   const url = `https://api.giphy.com/v1/gifs/random?api_key=${apiKey}&tag=lgtm&rating=g&lang=en`
-  const response = await fetch(url)
-  const responseJson = await response.json()
-  return responseJson.data.url
+  const { data } = await axios(url)
+  return data.data.url
 }
 
 export const checkFlawlessCode = (apiKey:string) => {

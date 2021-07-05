2e7a3bc2923006cf436b85ad74ae8fc4bc489de6
Roberto Hernández <roberto.hernandez@guidesmiths.com>
2021-07-05 13:46:20 +0200

    test: update danger and package version

--- src/rules/checkApprovals.ts
+++ src/rules/checkApprovals.ts
@@ -8,6 +8,7 @@ export declare function warn(message: string): void
 
 export const checkApprovals = () => {
   const prReviews = danger.github.reviews
+  console.log('reviews', prReviews)
   if (!prReviews.every(prReview => prReview.state === 'APPROVED')) {
     warn('You should not merge until everyone has approved the PR')
     return false
